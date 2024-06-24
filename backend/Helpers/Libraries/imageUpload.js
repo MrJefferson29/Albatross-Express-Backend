2204d35a0065abf86aa6cabad7ff69068fe1d157
MrJefferson29 <100526664+MrJefferson29@users.noreply.github.com>
2024-06-24 21:35:48 +0100

    Update imageUpload.js

--- backend/Helpers/Libraries/imageUpload.js
+++ backend/Helpers/Libraries/imageUpload.js
@@ -1,57 +1,59 @@
-const CustomError = require("../error/CustomError")
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const CustomError = require('../error/CustomError');
 
-const multer = require("multer")
-const path = require("path")
+// Function to ensure directory exists
+const ensureDirExists = (dir) => {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
+};
+
+// Function to sanitize filenames
+const sanitizeFilename = (filename) => {
+    return filename.replace(/[<>:"\/\\|?*\x00-\x1F\x7F]+/g, '_');
+};
 
 const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const rootDir = path.dirname(require.main.filename);
+        let dir;
 
-    destination : function(req,file,cb ){
-        const rootDir = path.dirname(require.main.filename) 
-        
-        if(file.fieldname === "photo"){
-            cb(null , path.join( rootDir,"/public/userPhotos"))
-        }
-        else {
-            cb(null , path.join( rootDir,"/public/storyImages"))
-        }
-
-    } ,
-
-    filename :function(req,file,cb ) {
-
-       
-        if(file.fieldname ==="photo"){
-            const extentions =file.mimetype.split("/")[1]
-           
-            req.savedUserPhoto ="photo_user_" +req.user.id + "."+ extentions
-
-            cb(null ,req.savedUserPhoto)
+        if (file.fieldname === "photo") {
+            dir = path.join(rootDir, "/public/userPhotos");
+        } else {
+            dir = path.join(rootDir, "/public/storyImages");
         }
 
-        else {
-            req.savedStoryImage ="image_" +new Date().toISOString().replace(/:/g, '-') + file.originalname 
+        ensureDirExists(dir);
+        cb(null, dir);
+    },
 
-            cb(null ,req.savedStoryImage)
+    filename: function (req, file, cb) {
+        const sanitizedOriginalname = sanitizeFilename(file.originalname);
+
+        if (file.fieldname === "photo") {
+            const extension = file.mimetype.split("/")[1];
+            req.savedUserPhoto = `photo_user_${req.user.id}.${extension}`;
+            cb(null, req.savedUserPhoto);
+        } else {
+            req.savedStoryImage = `image_${new Date().toISOString().replace(/:/g, '-')}_${sanitizedOriginalname}`;
+            cb(null, req.savedStoryImage);
         }
-
     }
+});
 
-})
-
-
-const fileFilter =(req,file,cb ) => {
-
-    allowedMimeTypes = ["image/jpeg","image/jpg","image/png","image/gif"]
+const fileFilter = (req, file, cb) => {
+    const allowedMimeTypes = ["image/jpeg", "image/jpg", "image/png", "image/gif", "image/heic", "image/heiv"];
 
     if (!allowedMimeTypes.includes(file.mimetype)) {
-        return (new CustomError("Please provide a valid image file ",400),null )
+        return cb(new CustomError("Please provide a valid image file", 400), false);
     }
-    
-    cb(null , true ) ;
-    
-}
 
-const imageUpload = multer({storage ,fileFilter  })
+    cb(null, true);
+};
 
+const imageUpload = multer({ storage, fileFilter });
 
-module.exports = imageUpload ; 
+module.exports = imageUpload;

aff12d291ec09d8c1ed048d7140a05e077110ec9
MrJefferson29 <100526664+MrJefferson29@users.noreply.github.com>
2024-06-26 13:12:15 +0100

    Update imageUpload.js

--- backend/Helpers/Libraries/imageUpload.js
+++ backend/Helpers/Libraries/imageUpload.js
@@ -1,33 +1,32 @@
-const CustomError = require("../error/CustomError");
-const multer = require("multer");
-const path = require("path");
+const multer = require('multer');
+const path = require('path');
+const CustomError = require('./path/to/CustomError');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         const rootDir = path.dirname(require.main.filename);
-        if (file.fieldname === "photo") {
-            cb(null, path.join(rootDir, "/public/userPhotos"));
+        if (file.fieldname === 'photo') {
+            cb(null, path.join(rootDir, '/public/userPhotos'));
         } else {
-            cb(null, path.join(rootDir, "/public/storyImages"));
+            cb(null, path.join(rootDir, '/public/storyImages'));
         }
     },
-
     filename: function(req, file, cb) {
-        if (file.fieldname === "photo") {
-            const extension = file.mimetype.split("/")[1];
-            req.savedUserPhoto = "photo_user_" + req.user.id + "." + extension;
+        if (file.fieldname === 'photo') {
+            const extension = file.mimetype.split('/')[1];
+            req.savedUserPhoto = 'photo_user_' + req.user.id + '.' + extension;
             cb(null, req.savedUserPhoto);
         } else {
-            req.savedStoryImage = "image_" + new Date().toISOString().replace(/:/g, '-') + file.originalname;
+            req.savedStoryImage = 'image_' + new Date().toISOString().replace(/:/g, '-') + file.originalname;
             cb(null, req.savedStoryImage);
         }
     }
 });
 
 const fileFilter = (req, file, cb) => {
-    const allowedMimeTypes = ["image/jpeg", "image/jpg", "image/png", "image/gif", "image/heic", "image/heiv"];
+    const allowedMimeTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'image/heic', 'image/heiv'];
     if (!allowedMimeTypes.includes(file.mimetype)) {
-        return cb(new CustomError("Please provide a valid image file", 400), false);
+        return cb(new CustomError('Please provide a valid image file', 400), false);
     }
     cb(null, true);
 };
